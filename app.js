7f4195d0cb5965aa4d0a45f7bf51b78a0e5fefcf
Maheshkumar <mtmaheshsairam84@gmail.com>
2020-05-24 17:35:52 +0530

    Fcc: serve and HTML file

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var express = require('express')
 var app = express()
 
 app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/views/index.html')
+})
+
+app.get('/', function (req, res) {
   res.send('Hello Express')
 })
 
